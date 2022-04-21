2778f8e798dafd7cf77921b1b607a07b5efa36b8
John Maras <sportsballenthusiast@gmail.com>
2022-04-21 13:31:49 -0500

    Went back and looked at undefined and still need to splice

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -12,19 +12,21 @@ console.log(
   addItem("shrimp")
 );
 console.log(basket);
-//Should just show my basket of shrimp
+// Should just show my basket of shrimp
 console.log(`True if adding bahn mi: `, addItem("bahn mi"));
 console.log(basket);
 
-//- Create a global variable named `basket` and set it to an empty array.
-//- Create a function called `addItem`. It should:
-//  - take an input parameter for a string `item`
-//  - add the new item to the global array `basket`.
-//  - return `true` indicating the item was added
+/* - Create a global variable named `basket` and set it to an empty array.
+ - Create a function called `addItem`. It should:
+ - take an input parameter for a string `item`
+ - add the new item to the global array `basket`.
+ - return `true` indicating the item was added
 
-//- Create a function called `listItems`. It should:
-// - loop over the items in the `basket` array
-//  - console.log each individual item on a new line
+ - Create a function called `listItems`. It should:
+ - loop over the items in the `basket` array
+ - console.log each individual item on a new line */
+
+/* WHY DOES THIS SHOW UP UNDEFINED? 
 function listItems() {
   for (let items of basket) {
     console.log(items);
@@ -32,22 +34,33 @@ function listItems() {
 }
 console.log(`These are the items in the basket: `, listItems());
 console.log(`These are the items in the basket:  ${listItems()}`);
-// NOTES to help me figure out what i'm doing
-// creating the function of list of items
-// for looping that i'm declaring items as my variable and I'm checking the basket for those variables.
-// i console log that variable items to get the list.
-// COME BACK TO WHY IT HAS UNDEFINED IN IT!
+*/
 
-//- Create a function called `empty`. It should:
-//  - reset the `basket` to an empty array
+function listItems(array) {
+  for (let i = 0; i <= array.length - 1; i++) {
+    console.log(array[i]);
+  }
+}
+console.log(listItems(basket));
 
+/* 
+ NOTES to help me figure out what i'm doing
+ - creating the function of list of items
+ - for looping that i'm declaring items as my variable and I'm checking the basket for those variables.
+ - i console log that variable items to get the list.
+
+
+- Create a function called `empty`. It should:
+  - reset the `basket` to an empty array */
+
+// BROKEN - LOOK UP ARRAY.SPLICE TO FIX IT
 function empty() {
   basket === [];
 }
 console.log(`Items in the basket: ${basket}`);
 //Showing items in basket first before you run the empty function
 empty();
-console.log(`Check out the array is now empty: `, empty());
+console.log(`Check out the array is now empty: `, basket);
 //Showing that you have now emptied it by console logging it
 
 //> __IMPORTANT__
